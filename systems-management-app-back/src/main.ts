a4235aa88453d4d2491fa3a0a7c09d27ee652591
Helio Soares <heliob_18@hotmail.com>
2023-10-02 12:18:45 -0300

    fix docker

--- systems-management-app-back/src/main.ts
+++ systems-management-app-back/src/main.ts
@@ -6,9 +6,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // whitelisted
-  // app.enableCors({
-  //   origin: 'http://localhost:3000',
-  // });
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
